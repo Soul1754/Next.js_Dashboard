37e0d6b6cffce172c14ee1588a982bdea731ff07
s0ulReaPeR <siddhant.gaikwad1754@gmail.com>
2025-10-02 11:58:40 +0530

    added customer overview section/ customer search filter

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return(<p>customers</p>)
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+
+      const searchParams = await props.searchParams;
+      const query = searchParams?.query || "";
+
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+        <CustomersTable customers={customers} />
+    </div>
+  );
 }
